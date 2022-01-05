7f3e52973826a6119beabc40d16bec6da62fc46a
Jaswant Chaudhary <91217491+jchy@users.noreply.github.com>
2022-01-05 23:14:29 +0530

    It is working fine

--- src/pages/TodoInput.jsx
+++ src/pages/TodoInput.jsx
@@ -1,30 +1,3 @@
-// import { createContext, useState } from "react";
-
-// export const AppContext = createContext();
-
-// function TodoInput({ onAdd }) {
-//   const [state, setState] = useState("");
-
-//   return (
-//     <div>
-//       <input
-//         value={state}
-//         onChange={(e) => setState(e.target.value)}
-//         placeholder="Search Repository On Github"
-//       />
-//       <button
-//         onClick={() => {
-//           onAdd(state);
-//           setState("");
-//         }}
-//       >
-//         SEARCH
-//       </button>
-//     </div>
-//   );
-// }
-
-// export default TodoInput;
 import { createContext, useState } from "react";
 
 export const AppContext = createContext();

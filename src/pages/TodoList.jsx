ad03690033b11ebcb8e4e078175ffbedadf5ad17
Jaswant Chaudhary <91217491+jchy@users.noreply.github.com>
2022-01-05 23:29:55 +0530

    bug is fixed

--- src/pages/TodoList.jsx
+++ src/pages/TodoList.jsx
@@ -1,35 +1,3 @@
-// import { useEffect } from "react";
-// import { useDispatch, useSelector, shallowEqual } from "react-redux";
-// import { getTodos } from "./api";
-
-// function TodoList() {
-//   const { todos, isLoading, isError } = useSelector(
-//     (state) => state.app,
-//     shallowEqual
-//   );
-
-//   const dispatch = useDispatch();
-//   useEffect(() => {
-//     dispatch(getTodos());
-//   }, []);
-
-//   return (
-//     <div>
-//       {isLoading && <h3>Loading...</h3>}
-//       {isError && <h3> Something went wrong!</h3>}
-//       {todos?.map((i) => (
-//         <div style={{ border: "1px solid black", gap: "2rem" }}>
-//           <p>{i.id}</p>
-//           <p>{i.node_id}</p>
-//           <p>Repository Name : {i.name}</p>
-//           <p>Description : {i.description}</p>
-//         </div>
-//       ))}
-//     </div>
-//   );
-// }
-
-// export default TodoList;
 import { useEffect, useState } from "react";
 import { useDispatch, useSelector, shallowEqual } from "react-redux";
 import { getTodos } from "./api";
@@ -54,11 +22,27 @@ function PrintRepo({ allFunc }) {
 
   return (
     <>
-      <div>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          gap: "2rem",
+          marginBottom: "20px",
+          marginTop: "20px"
+        }}
+      >
         {isLoading && <h3>Loading...</h3>}
         {isError && <h3> Something went wrong!</h3>}
         {todos?.map((i) => (
-          <div style={{ border: "1px solid black", gap: "2rem" }}>
+          <div
+            style={{
+              // display: "flex",
+              border: "1px solid black",
+              // gap: "2rem",
+              padding: "20px"
+              // flexDirection: "column"
+            }}
+          >
             <p>ID : {i.id}</p>
             <p>Repository Name : {i.name}</p>
             <p>NODE ID: {i.node_id}</p>
